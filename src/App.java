499f2bec2a905641fd8b4b41fc167edc589d2ea9
shoham bitton <shoham8988@gmail.com>
2024-02-05 15:29:53 +0200

    hi

---
+++ src/App.java
@@ -0,0 +1,24 @@
+class MyClass {
+void task() {
+        for (int i=0;i<10;i++){
+            System.out.println("doc-"+i+"-myclass");
+        }
+    }
+    }
+
+public class App {
+    public static void main(String[] args) {
+
+        System.out.println("111");
+
+        MyClass m=new MyClass();
+        m.task();
+
+        for (int i=0;i<10;i++){
+            System.out.println("doc-"+i+"-app");
+        }
+
+        System.out.println("2223");
+
+    }
+}
